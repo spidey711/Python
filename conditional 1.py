38e737fe9b58ecfaeefd042f5a73fea04c88999c
spidey711 <tshrma2004@gmail.com>
2021-07-18 16:24:37 +0530

    New question added

--- conditional 1.py
+++ conditional 1.py
@@ -1,11 +1,11 @@
 # Write a Python program to get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference
 
-def sub(num):
+def subtract(num):
     if num <= 17:
         return num-17
     else:
         return (num-17)*2
 
 num = float(input("Enter a number: "))
-res = sub(num)
+res = subtract(num)
 print(res)
