ac3c6499c24bc023a93e36dd3fbd0969c7daf10d
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2021-08-27 11:10:21 +0530

    New question added

--- file handling fun.py
+++ file handling fun.py
@@ -7,25 +7,26 @@ def method_read_file():
 def EXIT():
     print("Okay, exiting program now...")
     exit()
-mode = str(input("Enter mode: "))
-path = str(input("Enter path: "))
-filename = open(path, mode)
-if filename.writable() == True:
-    user_text = str(input("Enter some text: "))
-    filename.write()
-    print("File has been edited")
-    print(filename.read())
-if filename.writable() == False:
-    print("File cannot be edited")
-    proceed = str(input("Do you want to read the file? Y or N")).lower()
-    if proceed == 'Y':
+try:
+    mode = str(input("Enter mode: "))
+    path = str(input("Enter path: "))
+    filename = open(path, mode)
+    if filename.writable() == True:
+        user_text = str(input("Enter some text: "))
+        filename.write(user_text)
+        print("File has been edited")
+    elif filename.writable() == False:
+        print("File cannot be edited")
+        proceed = str(input("Do you want to read the file? Y or N ")).lower()
+        if proceed == 'y':
+            method_read_file()
+        elif proceed == "n":
+            EXIT()
+        else:
+            print("Please enter a valid option")
+    elif filename.readable() == True:
         method_read_file()
-    if proceed == "N":
-        EXIT()
     else:
-        print("Please enter a valid option")
-if filename.readable() == True:
-    method_read_file()
-else:
-    print("File cannot be read")
-    
+        print("File cannot be read")
+except ValueError or FileNotFoundError:
+    print("Please enter the name of an existing file and give a valid mode")
