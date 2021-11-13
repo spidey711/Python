cbe1ad14d130c753e4dc6414d4618308b90ab9f1
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2021-11-13 11:49:02 +0530

    New question added

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 # Write a Python program to display your details like name, age, address in three different lines
 
 def details(name, age, address):
-    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+    return ("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
 
 name = str(input("Enter name: "))
 age = int(input("Enter number: "))
 address = str(input("Enter address: "))
-details(name, age, address)
+print(details(name, age, address))
