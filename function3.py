558b7a570d7ec16935ff15d8189a06db504656d0
Tamonud Sharma <73441372+spidey711@users.noreply.github.com>
2021-11-16 20:46:42 +0530

    Delete function3.py

--- function3.py
+++
@@ -1,5 +0,0 @@
-# Write a program to create function calculation() such that it can accept two variables and calculate addition and subtraction. Also, it must return both addition and subtraction in a single return call.
-
-def calculation(a, b):
-    return a+b, a-b
-print(calculation(40,10))
