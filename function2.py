4a2aa1e5c94f000300b5e01fd557f848682768a5
Tamonud Sharma <73441372+spidey711@users.noreply.github.com>
2021-11-16 20:46:48 +0530

    Delete function2.py

--- function2.py
+++
@@ -1,5 +0,0 @@
-# Write a program to create function func1() to accept a variable length of arguments and print their value.
-def func1(*args):
-    return args
-print(func1(1,2,3,4,5))
-print(func1(6,7,8))
