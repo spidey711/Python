ba9568ed36812dcedecdc9ab02415466590dbcd6
Tamonud Sharma <73441372+spidey711@users.noreply.github.com>
2021-11-16 20:46:36 +0530

    Delete function4.py

--- function4.py
+++
@@ -1,9 +0,0 @@
-# Write a program to create a function show_employee() using the following conditions.
-
-#     It should accept the employee’s name and salary and display both.
-#     If the salary is missing in the function call then assign default value 9000 to salary
-
-def show_employee(name, salary=9000):
-    return f'Name: {name}: {salary}'
-print(show_employee('Ben',12000))
-print(show_employee('Jessa'))
