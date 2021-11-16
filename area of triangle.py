c3222536ceecec18e16507e114315b6c381570bb
Tamonud Sharma <73441372+spidey711@users.noreply.github.com>
2021-11-16 23:54:08 +0530

    Delete area of triangle.py

--- area of triangle.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program that will accept the base and height of a triangle and compute the area
-
-def area(height, base):
-    print((height*base)/2)
-
-base = float(input("Enter base value: "))
-height = float(input("Enter height: "))
-res = area(height, base)
-print(res)
