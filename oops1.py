ba404125cb12117cc1513113ba6b77f2eaf25011
Tamonud Sharma <73441372+spidey711@users.noreply.github.com>
2021-11-16 20:45:01 +0530

    Delete oops1.py

--- oops1.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
-class Vehicle:
-    def __init__(self, maxSpeed, mileage):
-        self.maxSpeed = maxSpeed
-        self.mileage = mileage
-
-car = Vehicle(240, 20)
-print(car.maxSpeed, car.mileage)
